8d55e9996bb970987ee98d2927444295ea0efba9
Gleici Pereira <gpcaresdev@gmail.com>
2022-08-31 19:23:27 -0500

    initial commit

---
+++ topword.cpp
@@ -0,0 +1,19 @@
+// Program identifies the most frequent word in a text file and outputs its frequency 
+// Created by Gleici Pereira on 08/29/2022
+
+
+#include <iostream>
+using namespace std;
+
+
+int main (){
+
+    string name;
+
+    cout << "Please type your name:";
+    cin >> name;
+
+    cout << "Hello, " << name << "!";
+    return 0;
+}
+
